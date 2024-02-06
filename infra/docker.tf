5b2fe44c725bcfd741eacc4d535b75cbeb6f16c5
Matheus Giovanni Lucena <contato@matheuslucena.dev>
2024-02-06 18:28:33 -0300

    fix docker build and push

--- infra/docker.tf
+++ infra/docker.tf
@@ -1,10 +1,6 @@
 resource "null_resource" "docker_push" {
   provisioner "local-exec" {
-    command = <<EOF
-	    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${local.region}.amazonaws.com
-	    docker build -t "${aws_ecr_repository.lambda.repository_url}:latest"  ../
-        docker push "${aws_ecr_repository.lambda.repository_url}:latest"
-	    EOF
+    command = "bash ${path.module}/docker.sh ../ ${aws_ecr_repository.lambda.repository_url}:latest"
   }
 
   triggers = {
