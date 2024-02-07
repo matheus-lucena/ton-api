be29ab2f3dd9e3ac0f606a49d560f668c8e45083
Matheus Giovanni Lucena <contato@matheuslucena.dev>
2024-02-07 09:14:55 -0300

    add dynamodb and cognito iam role

--- infra/iam.tf
+++ infra/iam.tf
@@ -19,3 +19,43 @@ resource "aws_iam_policy_attachment" "lambda_basic_access" {
   roles      = [aws_iam_role.lambda.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_policy_attachment" "cognito_access" {
+  name       = "cognito_access"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
+}
+
+
+resource "aws_iam_policy_attachment" "dynamodb_access" {
+  name       = "dynamodb_access"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.dynamodb.arn
+}
+
+resource "aws_iam_policy" "dynamodb" {
+  name        = "${var.name}-dynamo"
+  
+  policy = data.aws_iam_policy_document.data.dynamodb.json
+}
+
+data "aws_iam_policy_document" "dynamodb" {
+  statement {
+
+    actions = [
+      "dynamodb:Describe*",
+      "dynamodb:List*",
+      "dynamodb:Get*",
+      "dynamodb:DeleteItem",
+      "dynamodb:PartiQLSelect",
+      "dynamodb:UpdateTimeToLive",
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:PutItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem",
+    ]
+
+    resources = aws_dynamodb_table.*.arn
+  }
+}
