93dba66ac602b65f7d50fb0aef0395e37a2f0f72
Matheus Giovanni Lucena <contato@matheuslucena.dev>
2024-02-07 10:00:15 -0300

    add permission gateway lambda

--- infra/api.tf
+++ infra/api.tf
@@ -7,7 +7,7 @@ resource "aws_apigatewayv2_integration" "default" {
   api_id               = aws_apigatewayv2_api.main.id
   integration_type     = "AWS_PROXY"
   connection_type      = "INTERNET"
-  integration_method   = "POST"
+  integration_method   = "ANY"
   integration_uri      = aws_lambda_function.main.invoke_arn
   timeout_milliseconds = 30000
 }
@@ -19,8 +19,16 @@ resource "aws_apigatewayv2_route" "main" {
   target             = "integrations/${aws_apigatewayv2_integration.default.id}"
 }
 
-/*resource "aws_apigatewayv2_stage" "main" {
+resource "aws_apigatewayv2_stage" "main" {
   api_id = aws_apigatewayv2_api.main.id
   name   = "$default"
   auto_deploy = true
-}*/
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*"
+}
